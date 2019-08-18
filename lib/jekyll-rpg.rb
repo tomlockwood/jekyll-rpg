ae9d213a9934538e2435d216662f35a32d828b34
Tom Lockwood <tom.lockwood@bugcrowd.com>
2019-08-18 18:36:54 +1000

    More complete

--- lib/jekyll-rpg.rb
+++ lib/jekyll-rpg.rb
@@ -11,29 +11,46 @@ module JekyllRPG
     referent_page = doc.data['slug']
     referenced_collection = reference[/(?<=\/).*(?=\/)/]
     referenced_page = reference[/(?<=\/)(?:(?!\/).)*?(?=\))/]
-    site.data['references'] = {} unless site.data.key?('references')
+    @references = {} if @references.nil?
 
-    site.data['references'][referenced_collection] = {} unless site.data['references'].key?(referenced_collection)
-    site.data['references'][referenced_collection][referenced_page] = {} unless site.data['references'][referenced_collection].key?(referenced_page)
-    site.data['references'][referenced_collection][referenced_page][referent_collection] = [] unless site.data['references'][referenced_collection][referenced_page].key?(referent_collection)
+    @references[referenced_collection] = {} unless @references.key?(referenced_collection)
+    @references[referenced_collection][referenced_page] = {} unless @references[referenced_collection].key?(referenced_page)
+    @references[referenced_collection][referenced_page][referent_collection] = {} unless @references[referenced_collection][referenced_page].key?(referent_collection)
 
-    site.data['references'][referenced_collection][referenced_page][referent_collection].append({slug: referent_page, name: doc.data['name']})
+    @references[referenced_collection][referenced_page][referent_collection][referent_page] = doc.data['name']
   end
 
-  # Determine links between sites
+  # Bi-directional page links
   Jekyll::Hooks.register :site, :post_read do |site|
     collection_keys = site.collections.keys - ["posts"]
+
+    # On the site, build a full graph of references between collection pages
     collection_keys.each do |collection|
       site.collections[collection].docs.each do |doc|
-        doc.to_s.scan(/(?<=[^!])\[.*\]\(\/.*\/.*\)/).each do |reference|
+        doc.to_s.scan(/(?<=[^!])\[.*?\]\(\/.*?\/.*?\)/).each do |reference|
           add_reference(site, doc, reference)
         end
       end
     end
 
+    site.data['graph'] = @references
+
+    # For each collection page, add where it is referenced
     collection_keys.each do |collection|
       site.collections[collection].docs.each do |doc|
-        doc.data['referenced_by'] = site.data['references'][collection][doc.data['slug']]
+        slugs = {}
+        collection_keys.each do |add_keys|
+          slugs[add_keys] = []
+        end
+        unless @references[collection].nil? || @references[collection][doc.data['slug']].nil?
+          @references[collection][doc.data['slug']].each do |referent|
+            referent_collection = referent[0]
+            referent[1].each do |x|
+              slugs[referent_collection].append("[#{x[1]}](/#{referent_collection}/#{x[0]})")
+            end
+          end
+        end
+        doc.data['referenced_by'] = slugs
       end
     end
   end
